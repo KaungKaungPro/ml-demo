28c450fdd6ba54f8635728af3e037f8b7abe394b
KaungKaung. <e1237895@u.nus.edu>
2024-08-01 14:19:36 +0800

    Hello

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__) #changes
 def index():
     return 'Welcome to Fit Focus Machine Learning API!'
 
+@app.route('/hello')
+def hello():
+    return 'Hello!'
+
 if __name__ == "__main__":
     app.run()
